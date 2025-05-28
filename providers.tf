663378288a5edf46e1810be30800d6681c9c55a7
Garin Hughes <HughesG13@cardiff.ac.uk>
2025-05-28 11:14:02 +0100

    added k8s

--- providers.tf
+++ providers.tf
@@ -29,6 +29,13 @@ provider "azurerm" {
 
 provider "random" {}
 
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+}
+
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
